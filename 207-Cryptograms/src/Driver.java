846826949640988b832b2879310fa2245e7f4c30
Thomas <thomas.rampley.2019@uni.strath.ac.uk>
2021-03-05 00:39:45 +0000

    finished main method and added print encryption

--- 207-Cryptograms/src/Driver.java
+++ 207-Cryptograms/src/Driver.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class Driver {
 
 	public static void main(String[] args) {
+		boolean decided = false;
 		Game game = new Game();
 		try {
 			game.onStartup();
@@ -13,11 +14,23 @@ public class Driver {
 		}
 		Scanner myScan = new Scanner(System.in);
 		System.out.println("Welcome to Cryptogram!!");
-		System.out.println("Would you like a numbers or letters cryptogram?");
-		String type = myScan.nextLine();
-		if (type.equals("letters")) {
-			Cryptogram crypto = game.createLetters();
+		
+		Cryptogram crypto = game.decideCryptogram(myScan); //this had to be a seperate method otherwise it couldn't find it from if statements
+		game.printEncryption(crypto);
+		String responce;
+		while (true) {
+			System.out.println("What would you like to do? enter the number");
+			System.out.println("(1) make guess    (2) remove guess  (3) leave");
+			responce = myScan.nextLine();
+			if(responce.equals("1")) {
+				game.enterLetter(crypto); //this exists in another branch
+			} else if (responce.equals(2)) {
+				game.undoLetter(); //this also exists in another branch
+			} else if (responce.equals(3)) {
+				break;
+			} else {
+				System.out.println("I'm sorry that doesn't seem to be a valid input, please try again");
+			}
 		}
 	}
-
 }
--- 207-Cryptograms/src/Game.java
+++ 207-Cryptograms/src/Game.java
@@ -3,6 +3,7 @@ import java.util.concurrent.ThreadLocalRandom;
 import java.io.*;
 public class Game {
 	private ArrayList<String> phrases;
+	private Cryptogram cryptogram; //only used in the decide cryptogram function to avoid an error
 	public Game() {
 		ArrayList<String> phrases = new ArrayList<>();
 	}
@@ -15,17 +16,43 @@ public class Game {
 		}
 	}
 	
-	public Cryptogram createLetters() {
+	//helper method to create the cryptogram
+	private Cryptogram createLetters() {
 		Cryptogram crypto = new LettersCryptogram(phrases.get(ThreadLocalRandom.current().nextInt(1, 14)));
 		return crypto;
 	}
 	
-	public Cryptogram createNumbers() {
+	//helper method to create the cryptogram
+	private Cryptogram createNumbers() {
 		Cryptogram crypto;
 		return crypto;
 	}
 	
+	public Cryptogram decideCryptogram(Scanner scan) {
+		boolean decided = false;
+		while(!decided) {
+			System.out.println("Would you like a numbers or letters cryptogram");
+			String type = scan.nextLine();
+			if (type.toLowerCase().equals("letters")) {
+				cryptogram = createLetters();
+			} else if(type.toLowerCase().equals("numbers")) {
+				cryptogram = createNumbers();
+			} else {
+				System.out.println("Sorry that doesn't appear to be an option, would you like numbers or letters?");
+			}
+		}
+		return cryptogram;
+	}
+	
 	public void printEncryption(Cryptogram crypto) {
-		
+		String alphabet = "a b c d e f g h i j k l m n o p q r s t u v w x y z"; //alphabet better cylces through the letters
+		int[] frequencies = crypto.getFrequency();
+		System.out.println(crypto.getEncrypted());
+		System.out.println("Frequencies");
+		System.out.println(alphabet);
+		for (int i = 0; i < 26; i++) {
+			System.out.print(frequencies[i] + " ");
+		}
+		System.out.println(""); //extra line to break things up
 	} 
 }
