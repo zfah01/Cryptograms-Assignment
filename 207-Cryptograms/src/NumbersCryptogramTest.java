7b3a0f3e69d579650ac41d0122bab1f948728320
Thomas <thomas.rampley.2019@uni.strath.ac.uk>
2021-03-06 20:59:51 +0000

    finished the subclass tests

--- 207-Cryptograms/src/NumbersCryptogramTest.java
+++ 207-Cryptograms/src/NumbersCryptogramTest.java
@@ -1,15 +1,13 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.ArrayList;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-class LettersCryptogramTest {
+
+class NumbersCryptogramTest {
 	
 	//this makes sure that thes super class has been initualised, no need to test every function though
 	@Test
 	void checkphrasePassedthrough() {
-		LettersCryptogram crypt = new LettersCryptogram("test");
+		NumbersCryptogram crypt = new NumbersCryptogram("test");
 		assertEquals("test", crypt.getPhrase());
 	}
 	
@@ -18,7 +16,8 @@ class LettersCryptogramTest {
 	//but we can make sure its not the same as the phrase
 	@Test
 	void LettersEncryption() {
-		LettersCryptogram crypt = new LettersCryptogram("test");
+		NumbersCryptogram crypt = new NumbersCryptogram("test");
 		assertNotEquals("test", crypt.getEncrypted());
 	}
+
 }
