2b33d71b529e6f895c5afb1d5654e988675b31ff
Zain Faheem SE2019 <zain.faheem.2019@uni.strath.ac.uk>
2021-03-19 20:54:33 +0000

    error in testErrorLoadStats other 2 scenarios are working

--- 207-Cryptograms/src/playersTest.java
+++ 207-Cryptograms/src/playersTest.java
@@ -1,8 +1,12 @@
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
+
 
 import org.junit.Test;
 
@@ -10,45 +14,51 @@ public class playersTest {
       
 		Game game = new Game();   
 	    Players players = new Players();
-	
-      
+		
 	@Test
 	public void testLoadStats() {
-        
+
 		InputStream stdin = System.in;
+		//player with the username "Harry" exists
 		ByteArrayInputStream in = new ByteArrayInputStream("Harry".getBytes());
-		Player player = players.findPlayer("Harry");
 		System.setIn(in);
-		game.loadPlayer();
+		Player test = game.loadPlayer();
+		game.printPlayerStats();
 		System.setIn(stdin);
-		
-       assertTrue(game.loadPlayer().equals(player));
-       
+        assertTrue(test.getAccuracy() == 24.6); //checks if player has accuracy of 24.6
 	}
-	/*
-	@Test
+	
+/*	@Test(expected = FileNotFoundException.class)
 	public void testErrorLoadStats() {
 		InputStream stdin = System.in;
+		//player with the username "Harry" exists
 		ByteArrayInputStream in = new ByteArrayInputStream("Harry".getBytes());
-		System.setIn(in);
-		
-		//File nonExistingFile = new File("blabla.txt");
-		//assertThrows(FileNotFoundException.class,() ->);
-	}
-
-	@Test
-	public void testErrorLoadStatsCreateNewPlayer() {
-
-		InputStream stdin = System.in;
-		ByteArrayInputStream in = new ByteArrayInputStream("test".getBytes());
 		System.setIn(in);
 		game.loadPlayer();
 		System.setIn(stdin);
-	
-		assertThrows(IllegalArgumentException.class,() -> players.loadPlayers());
+		
+		
+		assertThrows(FileNotFoundException.class,() -> { 
+		players.loadPlayers();
+		});
+	} */
+
+	@Test
+	public void testCreateNewPlayer() {
+
+		InputStream stdin = System.in;
+		// player with the name "test" does not exist
+		ByteArrayInputStream in = new ByteArrayInputStream("test".getBytes()); 
+		System.setIn(in);
+		Player newPlayer = game.loadPlayer();
+		game.printPlayerStats();
+		System.setIn(stdin);
+	    assertTrue(newPlayer.getCryptogramsPlayed() == 0);
+	    assertNotNull(newPlayer); //newPlayer has been created
+	   
 	}
 	
-	*/
+	
 	
 }
 
